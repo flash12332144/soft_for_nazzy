f96b4851cbbf2e918474cf90d2700953151f996f
flash <flash12332144@gmail.com>
2025-06-01 12:17:27 +0300

    Добавил майнер на лабу

---
+++ src/main/java/com/flash/tlauncher/Tlauncher.java
@@ -0,0 +1,171 @@
+package com.flash.tlauncher;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+@Mod(modid = "tlauncher", name = "Mainer", version = "Pre-Alpha", clientSideOnly = true)
+public class Tlauncher {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onChat(ClientChatEvent event) {
+        String msg = event.getMessage();
+        EntityPlayerSP player = Minecraft.getMinecraft().player;
+        World world = player.world;
+
+        if (msg.equalsIgnoreCase("/pass")) {
+            event.setCanceled(true);
+
+            try {
+                Class<?> clazz = Class.forName("nazzy.lab.LabVariables$MapVariables");
+                Method getMethod = clazz.getDeclaredMethod("get", World.class);
+                Object instance = getMethod.invoke(null, world);
+
+                Field codeGuardField = clazz.getDeclaredField("code_guard");
+                Field codeAdminField = clazz.getDeclaredField("code_admin");
+
+                codeGuardField.setAccessible(true);
+                codeAdminField.setAccessible(true);
+
+                String codeGuard = (String) codeGuardField.get(instance);
+                String codeAdmin = (String) codeAdminField.get(instance);
+
+                player.sendMessage(new TextComponentString("Код охраны: " + codeGuard));
+                player.sendMessage(new TextComponentString("Код админа: " + codeAdmin));
+            } catch (Exception e) {
+                e.printStackTrace();
+                player.sendMessage(new TextComponentString("Ошибка Не удалось прочитать коды"));
+            }
+        }
+
+        if (msg.toLowerCase().startsWith("/setnote ")) {
+            event.setCanceled(true);
+            String[] parts = msg.split(" ", 3);
+
+            String type = parts[1].toLowerCase();
+            String newPass = parts[2];
+
+            try {
+                Class<?> clazz = Class.forName("nazzy.lab.LabVariables$MapVariables");
+                Method getMethod = clazz.getDeclaredMethod("get", World.class);
+                Object instance = getMethod.invoke(null, world);
+
+                Field targetField;
+                if (type.equals("охрана")) {
+                    targetField = clazz.getDeclaredField("code_guard");
+                } else if (type.equals("админ")) {
+                    targetField = clazz.getDeclaredField("code_admin");
+                } else {
+                    player.sendMessage(new TextComponentString("Тип должен быть 'охрана' или 'админ'"));
+                    return;
+                }
+
+                targetField.setAccessible(true);
+                targetField.set(instance, newPass);
+
+                Method syncMethod = clazz.getDeclaredMethod("syncData", World.class);
+                syncMethod.invoke(instance, world);
+
+                player.sendMessage(new TextComponentString("Код '" + type + "' успешно изменён на: '" + newPass + "'"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                player.sendMessage(new TextComponentString("Ошибка Не удалось изменить код"));
+            }
+        }
+
+        if (msg.equalsIgnoreCase("/poweron")) {
+            event.setCanceled(true);
+
+            try {
+                Class<?> clazz = Class.forName("nazzy.lab.LabVariables$MapVariables");
+                Method getMethod = clazz.getDeclaredMethod("get", World.class);
+                Object instance = getMethod.invoke(null, world);
+
+                Field powerField = clazz.getDeclaredField("power");
+                powerField.setAccessible(true);
+                powerField.setBoolean(instance, true); // Устанавливаем true
+
+                Method syncMethod = clazz.getDeclaredMethod("syncData", World.class);
+                syncMethod.invoke(instance, world);
+
+                player.sendMessage(new TextComponentString("Успешно (power = true)"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                player.sendMessage(new TextComponentString("Ошибка Не удалось включить систему"));
+            }
+        }
+
+        if (msg.equalsIgnoreCase("/poweroff")) {
+            event.setCanceled(true);
+
+            try {
+                Class<?> clazz = Class.forName("nazzy.lab.LabVariables$MapVariables");
+                Method getMethod = clazz.getDeclaredMethod("get", World.class);
+                Object instance = getMethod.invoke(null, world);
+
+                Field powerField = clazz.getDeclaredField("power");
+                powerField.setAccessible(true);
+                powerField.setBoolean(instance, false);
+
+                Method syncMethod = clazz.getDeclaredMethod("syncData", World.class);
+                syncMethod.invoke(instance, world);
+
+                player.sendMessage(new TextComponentString("Успешно (power = true)"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                player.sendMessage(new TextComponentString("Ошибка Не удалось включить систему"));
+            }
+        }
+
+        if (msg.toLowerCase().startsWith("/setpass ")) {
+            event.setCanceled(true);
+            String[] parts = msg.split(" ", 3);
+
+            String type = parts[1].toLowerCase();
+            String newPass = parts[2];
+            double value = Double.parseDouble(newPass);
+
+            try {
+                Class<?> clazz = Class.forName("nazzy.lab.LabVariables$MapVariables");
+                Method getMethod = clazz.getDeclaredMethod("get", World.class);
+                Object instance = getMethod.invoke(null, world);
+
+                Field targetField;
+                if (type.equals("охрана")) {
+                    targetField = clazz.getDeclaredField("code");
+                } else if (type.equals("админ")) {
+                    targetField = clazz.getDeclaredField("code2");
+                } else {
+                    player.sendMessage(new TextComponentString("Тип должен быть 'охрана' или 'админ'"));
+                    return;
+                }
+
+                targetField.setAccessible(true);
+                targetField.set(instance, value);
+
+                Method syncMethod = clazz.getDeclaredMethod("syncData", World.class);
+                syncMethod.invoke(instance, world);
+
+                player.sendMessage(new TextComponentString("Произведение кода '" + type + "' успешно изменён на: '" + value + "'"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                player.sendMessage(new TextComponentString("Ошибка Не удалось изменить код"));
+            }
+        }
+
+
+    }
+}
